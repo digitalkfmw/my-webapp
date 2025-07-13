fcb7626dd924da9f30cb437de1cf0aa494d4b6f7
digitalkfmw <digitalk.fmw@gmail.com>
2025-07-13 09:50:03 +0000

    Trigger CI/CD

--- deploy.py
+++ deploy.py
@@ -1,6 +1,14 @@
+# deploy.py
+
 import sys
-user, pwd, url, war, appName, target = sys.argv[1:]
-connect(user, pwd, url)
-deploy(appName, war, targets=target, upload='true')
+adminUrl = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+appPath = sys.argv[4]
+appName = sys.argv[5]
+
+connect(username, password, adminUrl)
+deploy(appName=appName, path=appPath, targets='AdminServer', upload='true')
 disconnect()
 exit()
+
